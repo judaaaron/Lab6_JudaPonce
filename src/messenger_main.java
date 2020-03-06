f50660324b7aa9a97057134f5fe9c880ddff1ac7
Juda <judaponce14@unitec.edu>
2020-03-06 15:00:35 -0600

    haciendo chat

--- src/messenger_main.java
+++ src/messenger_main.java
@@ -1,9 +1,15 @@
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Admin
@@ -15,6 +21,23 @@ public class messenger_main extends javax.swing.JFrame {
      */
     public messenger_main() {
         initComponents();
+
+        setResizable(false);
+        setTitle(" Messenger");
+        this.setLocationRelativeTo(null);
+
+        adminUsuarios ap = new adminUsuarios("./Users.txt");
+        ap.cargarArchivo();
+
+        for (Usuario p : ap.getPersonitas()) {
+            personas.add(p);
+
+        }
+
+        for (int i = 0; i < ap.getPersonitas().size(); i++) {
+            area2.append("\n" + ap.getPersonitas().get(i).getDisplay());
+
+        }
     }
 
     /**
@@ -26,6 +49,33 @@ public class messenger_main extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jd_registrarse = new javax.swing.JDialog();
+        jLabel7 = new javax.swing.JLabel();
+        jLabel8 = new javax.swing.JLabel();
+        jLabel9 = new javax.swing.JLabel();
+        jLabel10 = new javax.swing.JLabel();
+        bt_Registrar = new javax.swing.JButton();
+        bt_atras = new javax.swing.JButton();
+        tf_nombre = new javax.swing.JTextField();
+        tf_usuario = new javax.swing.JTextField();
+        tf_contra = new javax.swing.JPasswordField();
+        j_calendario = new com.toedter.calendar.JDateChooser();
+        jLabel11 = new javax.swing.JLabel();
+        jLabel12 = new javax.swing.JLabel();
+        lb_fondo2 = new javax.swing.JLabel();
+        chat = new javax.swing.JDialog();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        area1 = new javax.swing.JTextArea();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        area2 = new javax.swing.JTextArea();
+        tf_metodos = new javax.swing.JTextField();
+        bt_enviarMensaje = new javax.swing.JButton();
+        bt_guardarConversacion = new javax.swing.JButton();
+        bt_Salir2 = new javax.swing.JButton();
+        jLabel6 = new javax.swing.JLabel();
+        jLabel14 = new javax.swing.JLabel();
+        jLabel13 = new javax.swing.JLabel();
+        fondo3 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         tf_user = new javax.swing.JTextField();
@@ -34,7 +84,123 @@ public class messenger_main extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
         bt_salir = new javax.swing.JButton();
+        jLabel5 = new javax.swing.JLabel();
         jl_fondo = new javax.swing.JLabel();
+
+        jd_registrarse.getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel7.setFont(new java.awt.Font("Agency FB", 1, 24)); // NOI18N
+        jLabel7.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel7.setText("Registrarse");
+        jd_registrarse.getContentPane().add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 440, -1, -1));
+
+        jLabel8.setFont(new java.awt.Font("Agency FB", 1, 24)); // NOI18N
+        jLabel8.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel8.setText("Usuario");
+        jd_registrarse.getContentPane().add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 150, -1, -1));
+
+        jLabel9.setFont(new java.awt.Font("Agency FB", 1, 24)); // NOI18N
+        jLabel9.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel9.setText("Contraseña");
+        jd_registrarse.getContentPane().add(jLabel9, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 200, -1, -1));
+
+        jLabel10.setFont(new java.awt.Font("Agency FB", 1, 24)); // NOI18N
+        jLabel10.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel10.setText("Fecha de nacimiento");
+        jd_registrarse.getContentPane().add(jLabel10, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 250, -1, -1));
+
+        bt_Registrar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/forms.png"))); // NOI18N
+        bt_Registrar.setContentAreaFilled(false);
+        bt_Registrar.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                bt_RegistrarMouseClicked(evt);
+            }
+        });
+        jd_registrarse.getContentPane().add(bt_Registrar, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 350, 80, 80));
+
+        bt_atras.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/return64.png"))); // NOI18N
+        bt_atras.setContentAreaFilled(false);
+        bt_atras.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                bt_atrasMouseClicked(evt);
+            }
+        });
+        jd_registrarse.getContentPane().add(bt_atras, new org.netbeans.lib.awtextra.AbsoluteConstraints(280, 350, 70, 80));
+        jd_registrarse.getContentPane().add(tf_nombre, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 100, 120, -1));
+
+        tf_usuario.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                tf_usuarioActionPerformed(evt);
+            }
+        });
+        jd_registrarse.getContentPane().add(tf_usuario, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 150, 120, -1));
+        jd_registrarse.getContentPane().add(tf_contra, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 200, 110, -1));
+        jd_registrarse.getContentPane().add(j_calendario, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 260, 120, -1));
+
+        jLabel11.setFont(new java.awt.Font("Agency FB", 1, 24)); // NOI18N
+        jLabel11.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel11.setText("Atrás");
+        jd_registrarse.getContentPane().add(jLabel11, new org.netbeans.lib.awtextra.AbsoluteConstraints(300, 440, -1, -1));
+
+        jLabel12.setFont(new java.awt.Font("Agency FB", 1, 24)); // NOI18N
+        jLabel12.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel12.setText("Nombre");
+        jd_registrarse.getContentPane().add(jLabel12, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 100, -1, -1));
+
+        lb_fondo2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/wallMessenger.jpg"))); // NOI18N
+        jd_registrarse.getContentPane().add(lb_fondo2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 580, 500));
+
+        chat.getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        area1.setColumns(20);
+        area1.setRows(5);
+        jScrollPane1.setViewportView(area1);
+
+        chat.getContentPane().add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 10, 490, 470));
+
+        area2.setColumns(20);
+        area2.setRows(5);
+        jScrollPane2.setViewportView(area2);
+
+        chat.getContentPane().add(jScrollPane2, new org.netbeans.lib.awtextra.AbsoluteConstraints(510, 10, 200, 470));
+        chat.getContentPane().add(tf_metodos, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 490, 690, 80));
+
+        bt_enviarMensaje.setFont(new java.awt.Font("Agency FB", 1, 18)); // NOI18N
+        bt_enviarMensaje.setForeground(new java.awt.Color(255, 255, 255));
+        bt_enviarMensaje.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/plane.png"))); // NOI18N
+        bt_enviarMensaje.setContentAreaFilled(false);
+        bt_enviarMensaje.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                bt_enviarMensajeMouseClicked(evt);
+            }
+        });
+        chat.getContentPane().add(bt_enviarMensaje, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 570, 80, 80));
+
+        bt_guardarConversacion.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/document.png"))); // NOI18N
+        bt_guardarConversacion.setContentAreaFilled(false);
+        chat.getContentPane().add(bt_guardarConversacion, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 570, 80, 70));
+
+        bt_Salir2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/logout (1).png"))); // NOI18N
+        bt_Salir2.setContentAreaFilled(false);
+        chat.getContentPane().add(bt_Salir2, new org.netbeans.lib.awtextra.AbsoluteConstraints(610, 570, 80, 70));
+
+        jLabel6.setFont(new java.awt.Font("Agency FB", 1, 18)); // NOI18N
+        jLabel6.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel6.setText("Salir del chat");
+        chat.getContentPane().add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(600, 640, 80, -1));
+
+        jLabel14.setFont(new java.awt.Font("Agency FB", 1, 18)); // NOI18N
+        jLabel14.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel14.setText("Enviar Mensaje");
+        chat.getContentPane().add(jLabel14, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 640, -1, -1));
+
+        jLabel13.setFont(new java.awt.Font("Agency FB", 1, 18)); // NOI18N
+        jLabel13.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel13.setText("Guardar Conversación en archivos");
+        chat.getContentPane().add(jLabel13, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 640, -1, -1));
+
+        fondo3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/wlaw.jpg"))); // NOI18N
+        chat.getContentPane().add(fondo3, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, 670));
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
@@ -74,11 +240,125 @@ public class messenger_main extends javax.swing.JFrame {
         bt_salir.setSelectedIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/exit.png"))); // NOI18N
         getContentPane().add(bt_salir, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 360, 120, 100));
 
+        jLabel5.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel5.setText("¿No tienes cuenta?  Click here");
+        getContentPane().add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(640, 460, -1, -1));
+
         jl_fondo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/wallMessenger.jpg"))); // NOI18N
         getContentPane().add(jl_fondo, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 880, 500));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void tf_usuarioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_tf_usuarioActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_tf_usuarioActionPerformed
+
+    private void bt_atrasMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_bt_atrasMouseClicked
+        jd_registrarse.setVisible(false);
+    }//GEN-LAST:event_bt_atrasMouseClicked
+
+    private void bt_RegistrarMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_bt_RegistrarMouseClicked
+        String nombre = tf_nombre.getText();
+        String password = tf_contra.getText();
+        String display = tf_usuario.getText();
+        String calendario = j_calendario.getDate().toString();
+
+        personas.add(new Usuario(nombre, password, display, calendario));
+        boolean control = false;
+        for (int i = 0; i < personas.size(); i++) {
+            if (personas.get(i).getDisplay().equals(display)) {
+                JOptionPane.showMessageDialog(this, "Lo sentimos este nombre de usuario ya exite");
+                control = true;
+                tf_usuario.setText("");
+                break;
+
+            } else {
+                control = false;
+            }
+
+        }
+        if (control == false) {
+            adminUsuarios ap = new adminUsuarios("./Users.txt");
+            //ap.cargarArchivo();
+            Usuario p = new Usuario(nombre, password, display, calendario);
+            ap.getPersonitas().add(p);
+
+            try {
+                ap.escribirArchivo();
+            } catch (IOException ex) {
+                JOptionPane.showMessageDialog(this, ex);
+            }
+
+            JOptionPane.showMessageDialog(this, " Ha sido registrado exitosamente");
+
+            tf_user.setText("");
+            tf_contra.setText("");
+            tf_usuario.setText("");
+
+        }
+
+    }//GEN-LAST:event_bt_RegistrarMouseClicked
+
+    private void bt_enviarMensajeMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_bt_enviarMensajeMouseClicked
+        String cadena = tf_metodos.getText();
+        MetodosCadena m = new MetodosCadena();
+        int pos = cadena.indexOf(':');// uso del index of para saber su posicion y asi poder usar el metodo de substring
+        String nueva = cadena.substring(pos + 2, cadena.length());
+        String neww = cadena.substring(pos + 2, cadena.length() - 1);
+        if (nueva.length() < 13) {
+            JOptionPane.showMessageDialog(null, "La cadena debe de ser mayor a 13");
+        } else if (nueva.length() >= 13 && nueva.charAt(nueva.length() - 1) == '1') {
+            JOptionPane.showMessageDialog(null, "entro a la opcion1");
+            String nueva2 = m.concatenado(neww);
+            area1.setText("Usuario: " + neww + "\n" + "Maquina: " + nueva2);
+            tf_metodos.setText("");
+            String archi = JOptionPane.showInputDialog(null, "Escriba el nombre del archivo para guardar el chat");
+            System.out.println("./" + archi + ".txt");
+            ClaseMensajes ch = new ClaseMensajes("./" + archi + ".txt");
+            docs = "Usuario: " + neww + "Maquina: " + nueva2;
+            try {
+                ch.escribirArchivo(docs);
+            } catch (IOException ex) {
+              //  Logger.getLogger(MainMessenger.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+        } else if (nueva.length() >= 13 && nueva.charAt(nueva.length() - 1) == '2') {
+            JOptionPane.showMessageDialog(null, "entro a la opcion2");
+            String nn = m.ReplaceAscii(neww);
+
+            area1.setText("Usuario: " + neww + "\n" + "Maquina: " + nn + " ");
+            tf_metodos.setText("");
+            String archi = JOptionPane.showInputDialog(null, "Escriba el nombre del archivo para guardar el chat");
+            System.out.println("./" + archi + ".txt");
+            ClaseMensajes ch = new ClaseMensajes("./" + archi + ".txt");
+            docs = "Usuario: " + neww + "Maquina: " + nn;
+            try {
+                ch.escribirArchivo(docs);
+            } catch (IOException ex) {
+              //  --Logger.getLogger(MainMessenger.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+        } else if (nueva.length() >= 13 && nueva.charAt(nueva.length() - 1) == '3') {
+            JOptionPane.showMessageDialog(null, "entro a la opcion 3");
+            String nueva2 = m.patron3(neww);
+            area1.setText("Usuario: " + neww + "\n" + "Maquina: " + nueva2);
+            tf_metodos.setText("");
+            String archi = JOptionPane.showInputDialog(null, "Escriba el nombre del archivo para guardar el chat");
+            System.out.println("./" + archi + ".txt");
+            ClaseMensajes ch = new ClaseMensajes("./" + archi + ".txt");
+            docs = "Usuario: " + neww + "Maquina: " + nueva2;
+            try {
+                ch.escribirArchivo(docs);
+            } catch (IOException ex) {
+                //Logger.getLogger(MainMessenger.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } else if (nueva.length() >= 13 && nueva.charAt(nueva.length() - 1) == '4') {
+            JOptionPane.showMessageDialog(null, "entro a la opcion 4");
+        } else {
+            JOptionPane.showMessageDialog(null, "La cadena no posee ningun numero dentro del rango solicitado");
+        }
+    }//GEN-LAST:event_bt_enviarMensajeMouseClicked
 
     /**
      * @param args the command line arguments
@@ -116,14 +396,45 @@ public class messenger_main extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTextArea area1;
+    private javax.swing.JTextArea area2;
     private javax.swing.JButton bt_Ingresar;
+    private javax.swing.JButton bt_Registrar;
+    private javax.swing.JButton bt_Salir2;
+    private javax.swing.JButton bt_atras;
+    private javax.swing.JButton bt_enviarMensaje;
+    private javax.swing.JButton bt_guardarConversacion;
     private javax.swing.JButton bt_salir;
+    private javax.swing.JDialog chat;
+    private javax.swing.JLabel fondo3;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel10;
+    private javax.swing.JLabel jLabel11;
+    private javax.swing.JLabel jLabel12;
+    private javax.swing.JLabel jLabel13;
+    private javax.swing.JLabel jLabel14;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jLabel9;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private com.toedter.calendar.JDateChooser j_calendario;
+    private javax.swing.JDialog jd_registrarse;
     private javax.swing.JLabel jl_fondo;
+    private javax.swing.JLabel lb_fondo2;
+    private javax.swing.JPasswordField tf_contra;
+    private javax.swing.JTextField tf_metodos;
+    private javax.swing.JTextField tf_nombre;
     private javax.swing.JPasswordField tf_password;
     private javax.swing.JTextField tf_user;
+    private javax.swing.JTextField tf_usuario;
     // End of variables declaration//GEN-END:variables
+ArrayList<Usuario> personas = new ArrayList();
+    String docs;
+
 }
